5c1764cbd7ed42b2e7148de66cfc19727d84b0e3
Holden Karau <holden@pigscanfly.ca>
2021-03-06 01:31:29 +0000

    Put the scooter in the right place, configure static root

--- SparklingPinkPandas/settings.py
+++ SparklingPinkPandas/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  #'staticfiles'
+
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
